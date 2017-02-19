b03b5a9d87a3209db3431ae944d2feb6a3eea86c
Jean-Michel Vuillamy <cnjmv@wanadoo.fr>
2017-02-19 10:01:37 +0100

    Capture number of rotations every 5s
    Two major changes:
    - firstly, the EFM32 low energy pulse counter is used in order to count the number of rotations every 5s. 
    - secondly, the number of rotations is converted into km/h.

--- td12xx_template.c
+++ td12xx_template.c
@@ -12,12 +12,14 @@
 
 #include <efm32.h>
 #include <em_gpio.h>
+#include <em_pcnt.h>
 #include <td_measure.h>
 #include <td_core.h>
 #include <td_rtc.h>
 #include <td_gpio.h>
 #include <td_sigfox.h>
 
+uint8_t number_5S=0;
 uint8_t number_sample=0;
 uint8_t transmission_buffer[12];
 uint8_t sendmsg = 0;
@@ -26,37 +28,57 @@ uint8_t sendmsg = 0;
 #define ANEMOMETER_PORT	USR2_PORT				/**< Button port, previously was _PORT */
 #define ANEMOMETER_BIT	USR2_BIT				/**< Button bit */
 #define ANEMOMETER_MASK	USR2_MASK				/**< Button mask */
-uint8_t EveryMNTimer = 0xFF;
-uint32_t anemometer_counter=0;
+uint8_t Every5STimer = 0xFF;
 /* End of addition JMV */
 
-static void EveryMNFunction(uint32_t arg, uint8_t repetition){
-	// change line below - anemometer_counter * 0.067056 (to convert count into km/h)
-	transmission_buffer[number_sample] = (anemometer_counter) & 0xff;
-	anemometer_counter = 0;
-
-	if (number_sample==9) {
-		int32_t volt = TD_MEASURE_VoltageTemperatureExtended(false);
-		transmission_buffer[10] = ( volt >> 8 ) & 0xff;;
-		transmission_buffer[11] = ( volt ) & 0xff;
-		sendmsg = 1;
-		number_sample = 0;
+static void Every5SFunction(uint32_t arg, uint8_t repetition){
+	if (number_5S==0) {
+		transmission_buffer[number_sample] = 0;
 	} else {
-		number_sample = number_sample + 1;
+		transmission_buffer[number_sample] += (PCNT_CounterGet(PCNT0)) & 0xff;
+	}
+
+	PCNT_CounterTopSet(PCNT0, 0, 255);
+
+	if (number_5S==11) {
+		transmission_buffer[number_sample] = (uint8_t)(transmission_buffer[number_sample]*0.067056f);
+		if (number_sample==9) {
+			int32_t volt = TD_MEASURE_VoltageTemperatureExtended(false);
+			transmission_buffer[10] = ( volt >> 4 ) & 0xff;;
+			int32_t temp = TD_MEASURE_VoltageTemperatureExtended(true);
+			temp = (temp<-300)?-300:temp;
+			temp = (temp> 720)? 720:temp;
+			temp += 300;
+			transmission_buffer[11] = ( temp >> 2 ) & 0xff;
+			sendmsg = 1;
+			number_sample = 0;
+		} else {
+			number_sample = number_sample + 1;
+		}
+		number_5S = 0;
+	} else {
+		number_5S++;
 	}
 }
 
-static void AnemometerInterrupt(uint32_t mask){
-	anemometer_counter++;
-}
-
 void TD_USER_Setup(void){
-	int type;
-	int i;
-	IRQn_Type irq_parity;
+	uint8_t i;
 
-	// Setup TIM2_PORT as the LED I/O
+	CMU_ClockEnable(cmuClock_PCNT0, true);
+	GPIO_PinModeSet(ANEMOMETER_PORT, ANEMOMETER_BIT, gpioModeInputPullFilter, 1);
 	GPIO_PinModeSet(LED_PORT, LED_BIT, gpioModePushPull, 1);
+
+	PCNT_Init_TypeDef pcntInit =
+		  {
+		    .mode       = pcntModeOvsSingle,  /* clocked by LFACLK */
+		    .counter    = 0,                  /* Set initial value to 0 */
+		    .top        = 255,                 /* Set top to max value */
+		    .negEdge    = true,              /* positive edges */
+		    .countDown  = false,              /* up count */
+		    .filter     = true,               /* filter enabled */
+		  };
+	PCNT_Init(PCNT0, &pcntInit);
+	PCNT0->ROUTE = PCNT_ROUTE_LOCATION_LOC2;
 
 	// Blink the led 5 times at the beginning
 	for(i=0;i<6;i++) {
@@ -66,24 +88,8 @@ void TD_USER_Setup(void){
 		TD_RTC_Delay(T1S);
 	}
 
-	// Setup ANEMOMETER_PORT as the ANEMOMETER I/O
-	GPIO_PinModeSet(ANEMOMETER_PORT, ANEMOMETER_BIT, gpioModeInputPullFilter, 1);
-
-	// Set up a user hook on button pin interrupt
-	type = (ANEMOMETER_MASK & TD_GPIO_ODD_MASK) ?
-			TD_GPIO_USER_ODD : TD_GPIO_USER_EVEN;
-	TD_GPIO_SetCallback(type, AnemometerInterrupt, ANEMOMETER_MASK);
-	// Enable falling edge interrupts on button pin
-	GPIO_IntConfig(ANEMOMETER_PORT, ANEMOMETER_BIT, false, true, true);
-	// Clear and enable the corresponding interrupt in the CPU's Nested Vector
-	// Interrupt Controller
-	irq_parity =
-			(ANEMOMETER_MASK & TD_GPIO_ODD_MASK) ? GPIO_ODD_IRQn : GPIO_EVEN_IRQn;
-	NVIC_ClearPendingIRQ(irq_parity);
-	NVIC_EnableIRQ(irq_parity);
-
 	// Start 60s timer, which will trigger EveryMNFunction every minute
-	EveryMNTimer = TD_SCHEDULER_AppendIrq(60, 0 , 0, TD_SCHEDULER_INFINITE, EveryMNFunction, 0);
+	Every5STimer = TD_SCHEDULER_AppendIrq(5, 0 , 0, TD_SCHEDULER_INFINITE, Every5SFunction, 0);
 }
 
 void TD_USER_Loop(void){
